1020f9252861d4dc37be24e8ec0c39be67ebe9ee
岡遼 <oka1119ryo@gmail.com>
2019-02-06 23:25:40 +0900

    現在作成中のオリジナル2Dゲームです。 縦スクロールのゲームで上に登れば登るほどスコアが加算されていくゲームを予定しています。 障害物やコインによる得点の加算も予定しています。

---
+++ Assets/BackgroundController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour {
+
+	//Main
+	private GameObject MainCamera;
+
+	//スクロール速度
+	private float scrollSpeed = -0.30f;
+	//背景終了位置
+	//private float deadline = -48;
+	//背景開始位置
+	//private float StartLine = 5;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//背景を移動する
+		transform.Translate(0,this.scrollSpeed,0);
+
+		//画面外に出たら、画面に移動する
+		//if(transform.position.y < this.deadline) {
+		if (this.MainCamera.transform.position.y > this.gameObject.transform.position) {
+			transform.position = new Vector2 (0, this.MainCamera.transform.position.y);
+		}
+	}
+}
---
+++ Assets/GroundGenerator.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundGenerator : MonoBehaviour {
+	//足場のPrefab
+	public GameObject Field1Prefab;
+	public GameObject Field2Prefab;
+	public GameObject Field3Prefab;
+	public GameObject Field4Prefab;
+
+	//時間計測用の変数
+	private float delta = 0;
+
+	//足場の生成間隔
+	private float span = 1.0f;
+
+	//足場の生成位置:Y座標
+	private float genPosX = 12;
+
+	//足場の生成位置オフセット
+	private float offsetY = 0.3f;
+
+	//足場の縦方向の間隔
+	private float spaceY = 6.9f;
+
+	//足場の生成位置オフセット
+	private float offsetX = 0.5f;
+	//キューブの横方向の間隔
+	private float spaceX = 0.4f;
+
+	//足場の生成個数の上限
+	private int maxFieldNum =4;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.delta += Time.deltaTime;
+
+		//span秒以上の時間が経過したか調べる
+		if (this.delta > this.span) {
+			this.delta = 0;
+			//生成する足場数をランダムに決める
+			int n = Random.Range (1,maxFieldNum+1);
+
+			//指定した数だけ足場を生成する
+			for (int i = 0; i < n; i++) {
+				//足場の生成
+				GameObject go = Instantiate (Field1Prefab) as GameObject;
+				go.transform.position = new Vector2 (this.genPosX, this.offsetY + i * this.spaceY);
+			}
+			//次の足場までの生成時間を決める
+			this.span = this.offsetX + this.spaceX * n;
+		}
+	}
+}
